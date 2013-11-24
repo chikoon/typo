6b4b544eec6fffa54912fcbac5eaef52d15611ff
Mike Blake <chikoon@gmail.com>
2013-11-24 23:22:53 +0100

    no host and port in the form action please

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -42,7 +42,7 @@ class Admin::CategoriesController < Admin::BaseController
     if @category.id
       @form_action =  url_for(:action=>"edit", :id => @category.id, :only_path=>true)
     else
-      @form_action = url_for(:action=>'edit')
+      @form_action = url_for(:action=>'edit', :only_path=>true)
     end
     render 'new'
   end
